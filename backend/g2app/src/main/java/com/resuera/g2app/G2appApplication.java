f55c326b3d02e219dcbb95114a1a52fbd74a25c5
LAB24_USER ON WS37 <L24X07W37@LabHCI.net>
2026-02-09 08:15:54 +0800

    Backend Initialization: SpringBoot

---
+++ backend/g2app/src/main/java/com/resuera/g2app/G2appApplication.java
@@ -0,0 +1,13 @@
+package com.resuera.g2app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G2appApplication.class, args);
+	}
+
+}
---
+++ backend/g2app/src/test/java/com/resuera/g2app/G2appApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resuera.g2app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G2appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
